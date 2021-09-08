a1dc5a37db8457af1490c9335fe943c93fb2c660
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-09-08 21:22:00 +0300

    update

--- frontend/clientside/src/Components/Edit.js
+++ frontend/clientside/src/Components/Edit.js
@@ -19,7 +19,7 @@ const updateDescription = async e => {
   try {
     const body = { exercise,repetition,weight,duration };
     const response = await fetch(
-      `http://localhost:4000/todos/${item.tracker_id}`,
+      `http://localhost:4000/tracker/${item.tracker_id}`,
       {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
@@ -34,7 +34,92 @@ const updateDescription = async e => {
 };
   return (
     <div>
-      <button>Edit</button>
+            <button
+        type="button"
+        class="btn btn-warning"
+        data-toggle="modal"
+        data-target={`#id${item.tracker_id}`}
+      >
+        Edit
+      </button>
+
+      {/* 
+        id = id10
+      */}
+      <div
+        class="modal"
+        id={`id${item.tracker_id}`}
+        
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Edit Workout tracker</h4>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                onClick={() => setExercise(item.exercise)}
+              >
+                &times;
+              </button>
+            </div>
+
+            <div class="modal-body">
+              <label>Exercise</label>
+              <input
+                type="text"
+                className="form-control"
+                value={exercise}
+                onChange={e => setExercise(e.target.value)}
+              />
+
+              <label>Repetition</label>
+              <input
+                type="number"
+                className="form-control"
+                value={repetition}
+                onChange={e => setRepition(e.target.value)}
+              />    
+
+              <label>Weight</label>
+              <input
+                type="number"
+                className="form-control"
+                value={weight}
+                onChange={e => setWeight(e.target.value)}
+              />  
+
+            <label>Repetition</label>
+              <input
+                type="number"
+                className="form-control"
+                value={duration}
+                onChange={e => setDuration(e.target.value)}
+              />  
+            </div>
+
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-warning"
+                data-dismiss="modal"
+                onClick={e => updateDescription(e)}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger"
+                data-dismiss="modal"
+               
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       
     </div>
   )
